2b7f237799aa298f658c3852ef3d9ba1b56f67ad
perez816k <perez816pk@gmail.com>
2018-08-08 22:51:58 +0200

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  post '/signup',  to: 'users#create'
-  get  '/signup',  to: 'users#new'
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  post   '/signup',  to: 'users#create'
+  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
 end
